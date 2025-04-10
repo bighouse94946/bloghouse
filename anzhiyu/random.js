ad1d12166bd43b97a2b03e35d650bc0dbe1090e1
bighouse94946 <bighouse94946@gmail.com>
2025-04-10 17:48:18 +0800

    Site updated: 2025-04-10 17:48:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/09/当我听米店的时候，我在听些什么/"];function toRandomPost(){
+var posts=["2025/04/09/当我听米店的时候，我在听些什么/","2025/04/10/米店/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
